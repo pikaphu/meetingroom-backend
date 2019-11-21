ea6d0f819fbf924c3b4795a0213be64da40c4f3f
PikaPhu Freedom <whitehunter100@gmail.com>
2019-11-21 20:40:07 +0700

    Update just improve comments

--- config/my-override-config.js
+++ config/my-override-config.js
@@ -1,16 +1,16 @@
 // JUST OVERRIDE with My Config
 
-// override db
+// override db - mysql
 if (process.env.MY_DB_HOST != null) {
     global.myConfig.mysql.host = process.env.MY_DB_HOST
 }
 
-// override host
+// override API server 
 if (process.env.MY_APP_HOST_PORT != null) {
     global.myConfig.server_port = process.env.MY_APP_HOST_PORT
 }
 
-// override host
+// override FTP
 if (process.env.MY_FTP_HOST != null) {
     global.myConfig.ftp.host = process.env.MY_FTP_HOST
     global.myConfig.ftp.user = process.env.MY_FTP_USER
