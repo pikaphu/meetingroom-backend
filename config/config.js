5e56c90612b59aca1dee4ae748f39aeeb3e1f4da
PikaPhu Freedom <whitehunter100@gmail.com>
2019-12-03 15:59:33 +0700

    Add ENV config

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
     // if not production build then use config from .env file
     require('dotenv').config() // dev, local
 }
+console.log('Load ENV, OVERRIDE:', process.env.NODE_ENV, process.env.NODE_ENV_OVERRIDE);
 
 // 3. load main environment 
 const config = require('./config.json'); // get main config file
@@ -17,8 +18,23 @@ const environmentConfig = config[environment]; // select using config from app e
 let finalConfig = _.merge(defaultConfig, environmentConfig); // Now we have default config and loaded config
 
 // 4. load another environment
-const dbEnv = 'database.' + environment + '.json'
-const dbconfig = require('./' + dbEnv)
+var dbEnv, dbconfig
+try {
+    // load target env
+    dbEnv = 'database.' + environment + '.json'
+    dbconfig = require('./' + dbEnv)
+
+} catch (error) {
+    // if cannot load file then use default
+    console.log('DB Config Error-1! try dev:');
+    try {
+        dbEnv = 'database.development.json'
+        dbconfig = require('./' + dbEnv)
+    } catch (error) {
+        console.log('DB Config Error-2! none');
+    }
+}
+
 finalConfig = _.merge(finalConfig, dbconfig)
 
 // Final. setup config
@@ -28,6 +44,6 @@ global.myConfig = finalConfig;
 // console.log(`global.gConfig: ${JSON.stringify(global.myConfig, undefined, global.myConfig.json_indentation)}`);
 
 // -- Additional config for my machine --
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV_OVERRIDE && process.env.NODE_ENV !== 'production') {
     require('./my-override-config.js') // override current config
 }
